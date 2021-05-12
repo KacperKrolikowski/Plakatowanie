6c31b3c1b4940ace5d9eb661ba18ce4f7ef20058
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-12 20:43:41 +0200

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,34 +9,53 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int buildingQuantity = scanner.nextInt();
-        int[][] buildings = new int[buildingQuantity][2];
+        int width = 0;
+        int height = 0;
 
-        boolean isBetween = false;
+        int pointer = 0;
 
         int neededPosters = 1;
 
         int [] heightPlan = new int[buildingQuantity];
 
-        for(int i = 0; i < buildingQuantity; i++){
+        width = scanner.nextInt();
+        heightPlan[0] = scanner.nextInt();
 
-            buildings[i][0] = scanner.nextInt();
-            buildings[i][1] = scanner.nextInt();
-            heightPlan[i] = buildings[i][1];
+        for(int i = 1; i < buildingQuantity; i++){
+
+            width = scanner.nextInt();
+            height = scanner.nextInt();
+            while (height < heightPlan[pointer]){
+                neededPosters++;
+                pointer--;
+                if (pointer == -1){
+                    heightPlan[pointer+=1] = height;
+                }
+            }
+            if(height > heightPlan[pointer]){
+                heightPlan[pointer+=1] = height;
+            }
         }
 
-        for (int i = 1; i < buildingQuantity; i++){
-            if(heightPlan[i] != heightPlan [i - 1]){
-                for(int j = 0; j < i; j++){
-                    if (heightPlan[j] < heightPlan[i]){
-                        isBetween = true;
+        neededPosters += pointer;
+
+
+        /*for(int i = 1; i < buildingQuantity; i++){
+            if(heightPlan[i] != heightPlan[i - 1]){
+                if (heightPlan[i] > heightPlan[i - 1]){
+                    neededPosters++;
+                }else if (heightPlan[i] < heightPlan[i - 1]){
+                    for(int j = 1; j <= i; j++){
+                        if (heightPlan[i - j] == heightPlan[i]){
+                            break;
+                        }else if (heightPlan[i - j] < heightPlan[i]){
+                            neededPosters++;
+                            break;
+                        }
                     }
                 }
             }
-            if(isBetween == true){
-                neededPosters++;
-                isBetween = false;
-            }
-        }
+        } */
 
         System.out.println(neededPosters);
 
